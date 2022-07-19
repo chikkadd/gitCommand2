16518e56705b36c1618392edf9629d66f2844fff
Chikke <chikkadd20@gmail.com>
2022-07-19 21:50:45 +0530

    3rd code

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,7 +2,9 @@ package com.test;
 
 public class Test 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("_______________");
 		System.out.println("Hello");
 	}
 
